d346c3d164b9b1e5c15ca61219cf6d034e2e44fd
Martin Makarsky <martin.makarsky@fingerprint.com>
2025-08-04 11:33:04 +0200

    fix: remove unused GitHub provider

--- fastly-vcl-via-official-module/main.tf
+++ fastly-vcl-via-official-module/main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">=1.5"
-  
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = ">= 6.0.0"
-    }
-  }
 }
 
 module "fingerprint_fastly_vcl_integration" {
